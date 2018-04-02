e18998fbe98aec61e8361d653f24e5f93a79e28b
Bobby Karabinakis <bkarabinakis@gmail.com>
2018-04-01 21:52:46 -0700

    added version

--- externalUrlForLangSwitcher.php
+++ externalUrlForLangSwitcher.php
@@ -1,7 +1,11 @@
 <?php 
+/**
+*Plugin Name: External URL for Lang Switcher
+*Description: Adds the ability to link a specific language to an external URL used with WPML
+*Version: 0.0.1 Beta
+**/
 
 
-//adds the ability to link a specific language to an external URL used with WPML
 // add your desire language code line 9
 //add your desired url line 10
 function edit_wpml_lang( $url = '', $language = '' ) {
